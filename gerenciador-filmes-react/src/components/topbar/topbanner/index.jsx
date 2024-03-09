fefe1f0bec10201e25f84b7adce352ae4eefe9b4
luarxx <raullennon@raulrockclub.com.br>
2024-03-08 23:54:48 -0300

    add logo responsivo no topbanner

--- gerenciador-filmes-react/src/components/topbar/topbanner/index.jsx
+++ gerenciador-filmes-react/src/components/topbar/topbanner/index.jsx
@@ -1,8 +1,10 @@
 import "../topbanner/topbannerstyle.css"
+import LogoDoSite from "./imgs/logo.png"
 
 export function Topbanner() {
     return (
         <div className="topbanner">
+            <img src={LogoDoSite} alt="Logo do site" />
             <center >GERENCIADOR DE FILMES</center>
         </div>
     )
