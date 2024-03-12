cf894cdb207e4a18c3e812f2f39d5cea691ef327
luarxx <raullennon@raulrockclub.com.br>
2024-03-12 17:28:24 -0300

    att do componente menu

--- gerenciador-filmes-react/src/components/menu/index.jsx
+++ gerenciador-filmes-react/src/components/menu/index.jsx
@@ -1,10 +1,21 @@
 import "./menustyle.css"
+import { Link } from "react-router-dom";
 
 export function Menu() {
     return (
         <div className="menu">
-            Home
-            
+            <nav className="menu">
+                <Link to="/" className="menu">Home</Link>
+            </nav>
+            <nav className="menu">
+                <Link to="/" className="menu">Filmes</Link>
+            </nav>
+            <nav className="menu">
+                <Link to="/" className="menu">Diretores</Link>
+            </nav>
+            <nav className="menu">
+                <Link to="/" className="menu">Atores</Link>
+            </nav>
         </div>
     )
 }
